52ec72ea5f14254a0b6e3f1c3c3b4ca2b358fb9d
Griffith88 <dobrvn@gmail.com>
2020-07-10 15:11:46 +0300

    s
    s

--- week1/solution.py
+++ week1/solution.py
@@ -1,2 +1,16 @@
-c = dict(zip('([{', ')]}'))
-print(c)
+def modify_list(l):
+    length = len(l)
+    for i in range(length):
+        if i >= length:
+            break
+        if l[i] % 2 == 1:
+            del l[i]
+            length -=1
+            i += 1
+        else:
+            l[i] //= 2
+
+
+array = [1, 3, 5]
+modify_list(array)
+print(array)
